6346e1307aed712466a87ce6fb060a0a5bc03134
Celeste Aguilar <111248716+MadMax2808@users.noreply.github.com>
2025-05-12 03:39:14 -0600

    -Solo cambios en -inputComponet-

--- Frontend/src/Components/inputComponent.jsx
+++ Frontend/src/Components/inputComponent.jsx
@@ -1,15 +1,19 @@
 function InputComponent(props) {
-    
-    const { label, type, name, id, className } = props
-    
-    return(
+    const { label, type, name, id, className, value, onChange } = props;
+
+    return (
         <>
-            <label htmlFor="">{label}</label>
-            <input type={type} name={name} id={id} className={className}>
-            </input>
+            <label htmlFor={id}>{label}</label>
+            <input
+                type={type}
+                name={name}
+                id={id}
+                className={className}
+                value={value} // Propaga el valor
+                onChange={onChange} // Propaga el evento onChange
+            />
         </>
-    )
+    );
 }
 
-export default InputComponent
-            
+export default InputComponent;
